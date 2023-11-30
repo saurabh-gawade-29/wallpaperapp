fb0fb5cec03737247384bf48af08b5dbdac362af
saurabh gawade <saurabhgawade@finacus.co.in>
2023-11-30 20:34:09 +0530

    Temp About Create

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,116 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <div className="w-full p-4 text-start bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
+          Educational Purpose
+        </h5>
+        <p className="mb-1 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+          Utilizing the Pixabay API, a wallpaper app
+        </p>
+        <p className="mb-1 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+          You can use and download these fantastic photos.
+        </p>
+      </div>
+
+      <div className="grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2 bg-white dark:bg-gray-800 my-4">
+        <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
+          <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              Awesome App
+            </h3>
+            <p className="my-4">
+              If you care for your time, I hands down would go with this."
+            </p>
+          </blockquote>
+          <figcaption className="flex items-center justify-center ">
+            <img
+              className="rounded-full w-9 h-9"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png"
+              alt="profile picture"
+            />
+            <div className="space-y-0.5 font-medium dark:text-white text-left rtl:text-right ms-3">
+              <div>Bonnie Green</div>
+              <div className="text-sm text-gray-500 dark:text-gray-400 ">
+                Developer at Open AI
+              </div>
+            </div>
+          </figcaption>
+        </figure>
+        <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 md:rounded-se-lg dark:bg-gray-800 dark:border-gray-700">
+          <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              Must Use this Wallpaper site
+            </h3>
+            <p className="my-4">
+              Download now
+            </p>
+          </blockquote>
+          <figcaption className="flex items-center justify-center ">
+            <img
+              className="rounded-full w-9 h-9"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png"
+              alt="profile picture"
+            />
+            <div className="space-y-0.5 font-medium dark:text-white text-left rtl:text-right ms-3">
+              <div>Roberta Casas</div>
+              <div className="text-sm text-gray-500 dark:text-gray-400">
+                Lead designer at Dropbox
+              </div>
+            </div>
+          </figcaption>
+        </figure>
+        <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 md:rounded-es-lg md:border-b-0 md:border-e dark:bg-gray-800 dark:border-gray-700">
+          <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              Minimal Wallpapers are Awesome
+            </h3>
+            <p className="my-4">
+              Aesthetically, the well designed wallpaper are beautiful and will
+              undoubtedly level up your desktop"
+            </p>
+          </blockquote>
+          <figcaption className="flex items-center justify-center ">
+            <img
+              className="rounded-full w-9 h-9"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
+              alt="profile picture"
+            />
+            <div className="space-y-0.5 font-medium dark:text-white text-left rtl:text-right ms-3">
+              <div>Jese Leos</div>
+              <div className="text-sm text-gray-500 dark:text-gray-400">
+                Software Engineer at Facebook
+              </div>
+            </div>
+          </figcaption>
+        </figure>
+        <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-gray-200 rounded-b-lg md:rounded-se-lg dark:bg-gray-800 dark:border-gray-700">
+          <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              Nice App Overall
+            </h3>
+            <p className="my-4">
+              You have many examples that can be used in your personal projects and
+              for your team."
+            </p>
+          </blockquote>
+          <figcaption className="flex items-center justify-center ">
+            <img
+              className="rounded-full w-9 h-9"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png"
+              alt="profile picture"
+            />
+            <div className="space-y-0.5 font-medium dark:text-white text-left rtl:text-right ms-3">
+              <div>Joseph McFall</div>
+              <div className="text-sm text-gray-500 dark:text-gray-400">
+                CTO at Google
+              </div>
+            </div>
+          </figcaption>
+        </figure>
+      </div>
+    </>
   );
 };
 
