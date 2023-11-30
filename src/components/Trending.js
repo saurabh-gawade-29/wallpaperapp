e842182feadc623554c78b57f4daac6782b355c5
Saurabh Gawade <saurabhgawade2920@gmail.com>
2023-11-30 22:58:03 +0530

    API integration done

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,6 +1,35 @@
+import axios from "axios";
 import React from "react";
+import { useEffect, useState } from "react";
 
 const Trending = () => {
+  //! Use Effect
+  const [wallpapers, setWallpapers] = useState([]);
+  //! Functions
+  const getWallpaper = () => {
+    debugger;
+    axios
+      .get(
+        "https://pixabay.com/api/?key=40987026-d6665d5f7d2e023b7e3287980&image_type=photo"
+      )
+      .then((response) => {
+        console.log(response, "Wallpapers");
+        let beforeSorting = response.data.hits;
+        let afterSorting = beforeSorting.sort((a, b) => {
+          return b.downloads - a.downloads;
+        });
+        const top2Records = afterSorting.slice(0, 10);
+        setWallpapers(top2Records);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  //! EntryPoint
+  useEffect(() => {
+    debugger;
+    getWallpaper();
+  }, []);
   return (
     <>
       <div className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 my-4">
@@ -14,76 +43,17 @@ const Trending = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            className="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg"
-            alt=""
-          />
-        </div>
+        {wallpapers.map((element, i) => {
+          return (
+            <div key={i}>
+              <img
+                className="h-auto max-w-full rounded-lg w-full h-full object-fill"
+                src={element.webformatURL}
+                alt=""
+              />
+            </div>
+          );
+        })}
       </div>
     </>
   );
--- src/components/Wallpaper.js
+++ src/components/Wallpaper.js
@@ -8,7 +8,7 @@ const Wallpaper = () => {
   const getWallpaper = () => {
     axios
       .get(
-        "https://pixabay.com/api/?key=40987026-d6665d5f7d2e023b7e3287980&q=yellow+flowers&image_type=photo"
+        "https://pixabay.com/api/?key=40987026-d6665d5f7d2e023b7e3287980&image_type=photo"
       )
       .then((response) => {
         console.log(response, "Wallpapers");
@@ -20,18 +20,19 @@ const Wallpaper = () => {
   };
 
   useEffect(() => {
+    debugger;
     getWallpaper();
   }, []);
 
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-6 lg:grid-cols-6 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-5 gap-4">
         {wallpapers.map((element, i) => {
           return (
             <div key={i}>
               <img
-                className="h-auto max-w-full rounded-lg"
-                src={element.previewURL}
+                className="h-auto max-w-full rounded-lg w-full h-full object-fill"
+                src={element.webformatURL}
                 alt=""
               />
             </div>
