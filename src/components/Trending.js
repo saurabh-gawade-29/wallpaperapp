54974755c062f037361a39e1afb83af1eda81f4f
saurabh gawade <saurabhgawade@finacus.co.in>
2023-12-01 13:29:38 +0530

    download icon added

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { serviceCallGet } from "../Helper/Service";
+import downloadImg from "../Assets/img/downloads.png";
 
 let baseURL = `${process.env.REACT_APP_API_URL}?key=${process.env.REACT_APP_API_KEY}&image_type=photo&per_page=200&safesearch=false&safesearch=true`;
 
 const Trending = () => {
   //! Use Effect
   const [wallpapers, setWallpapers] = useState([]);
+  const [modal, setModal] = useState(false);
   //! Functions
   const getWallpaper = async () => {
     debugger;
@@ -17,6 +19,10 @@ const Trending = () => {
     });
     let topTen = afterSorting.slice(0, 12);
     setWallpapers(topTen);
+  };
+
+  const openModal = () => {
+    setModal(!modal);
   };
   //! EntryPoint
   useEffect(() => {
@@ -38,15 +44,97 @@ const Trending = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
         {wallpapers.map((element, i) => {
           return (
-            <div key={i}>
+            <div key={i} className="relative eyeParent">
               <img
                 className="h-auto max-w-full rounded-lg w-full h-full object-cover"
                 src={element.webformatURL}
                 alt=""
               />
+              <div className="absolute right-2 top-2 eyeIcon">
+                <button
+                  className="block text-white focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center w-20"
+                  data-modal-target="default-modal"
+                  data-modal-toggle="default-modal"
+                  onClick={openModal}
+                >
+                  <img src={downloadImg} alt="" />
+                </button>
+              </div>
             </div>
           );
         })}
+      </div>
+
+      <div
+        style={{ display: modal && "flex" }}
+        id="default-modal"
+        tabIndex="-1"
+        aria-hidden="true"
+        className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      >
+        <div className="relative p-4 w-full max-w-2xl max-h-full">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                Terms of Service
+              </h3>
+              <button
+                onClick={openModal}
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                data-modal-hide="default-modal"
+              >
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+            </div>
+
+            <div className="p-4 md:p-5 space-y-4">
+              <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                With less than a month to go before the European Union enacts
+                new consumer privacy laws for its citizens, companies around the
+                world are updating their terms of service agreements to comply.
+              </p>
+              <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                The European Union’s General Data Protection Regulation
+                (G.D.P.R.) goes into effect on May 25 and is meant to ensure a
+                common set of data rights in the European Union. It requires
+                organizations to notify users as soon as possible of high-risk
+                data breaches that could personally affect them.
+              </p>
+            </div>
+
+            <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+              <button
+                data-modal-hide="default-modal"
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                I accept
+              </button>
+              <button
+                data-modal-hide="default-modal"
+                onClick={openModal}
+                className="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+              >
+                Decline
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
