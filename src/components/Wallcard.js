5c69e2ee706bdee233af9dbce1dfa9f411c2eb86
saurabh gawade <saurabhgawade@finacus.co.in>
2023-12-02 16:39:47 +0530

    update

--- src/components/Wallcard.js
+++ src/components/Wallcard.js
@@ -72,7 +72,7 @@ const Wallcard = ({ data }) => {
         className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
       >
         <div className="relative p-4 w-full max-w-2xl max-h-full">
-          <div className="relative rounded-lg shadow dark:bg-gray-700">
+          <div className="relative bg-gray-700 rounded-lg shadow dark:bg-gray-700">
             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
               <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                 Free for use under the Pixabay Content License
