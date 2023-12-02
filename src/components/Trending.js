34c9df2c0e6b39f65304ca8d75c8600fa597a454
saurabh gawade <saurabhgawade@finacus.co.in>
2023-12-02 16:41:01 +0530

    update

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -28,7 +28,7 @@ const Trending = () => {
   }, []);
   return (
     <>
-      <div className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 my-4">
+      <div className="block p-6 bg-gray-800 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-4">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Here You Get TOP 21 Editor's Choice Wallapers
         </h5>
