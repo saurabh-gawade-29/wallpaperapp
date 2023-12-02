cef8eb3932b7661fa5fb07961f8ddf667156b25e
Saurabh Gawade <saurabhgawade2920@gmail.com>
2023-12-02 23:06:58 +0530

    update

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { serviceCallGet } from "../Helper/Service";
 import Wallcard from "./Wallcard";
+import { Link } from "react-router-dom";
 
 let baseURL = `${process.env.REACT_APP_API_URL}?key=${process.env.REACT_APP_API_KEY}&image_type=photo&per_page=200&editors_choice=true`;
 
@@ -42,6 +43,26 @@ const Trending = () => {
           Choosing an app depends on the type of wallpapers you prefer - dark,
           minimal, Kawaii, stock, AMOLED, etc. Most apps are free
         </p>
+        <Link to="/wallpaper" className="cursor-progress">
+          <button className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-2">
+            Search
+            <svg
+              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </button>
+        </Link>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
--- src/components/Wallpaper.js
+++ src/components/Wallpaper.js
@@ -75,7 +75,7 @@ const Wallpaper = () => {
             />
             <button
               onClick={(e) => handleURL(e)}
-              className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white absolute end-2.5 bottom-2.5 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
             >
               Search
             </button>
